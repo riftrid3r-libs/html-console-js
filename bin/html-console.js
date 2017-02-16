2b347b77bb4b04c4f441ff52a49f826268d35141
dib <dib@dib.p>
2017-02-15 20:38:33 -0800

    try some fix for JSFiddle

--- bin/html-console.js
+++ bin/html-console.js
@@ -12,7 +12,7 @@ doc: css: .cline{}
      html: <div id="clog"></div>
      
 */
-$( document ).ready(function(){
+$( document ).load(function(){
   var resultPage = "<div class=\"wrapper\"><div class=\"console-tabs\"><span id=\"consoleButton\" class=\"tab selected\">Console</span><span id=\"hButton\" class=\"tab\">Render</span></div><div id=\"clog\" class=\"console-page\"></div></div>";
   $('body').prepend( resultPage )
 
