551b0785fffb0b7e958d7aaad281e3f569512843
dib <dib@dib.p>
2017-02-15 19:46:19 -0800

    add compatbility with JSFiddle.net

--- bin/html-console.js
+++ bin/html-console.js
@@ -7,13 +7,15 @@ S       O         L         E.js
 Author: Drew Billings
 Description: console output in jsfiddle.
 Copyright: Drew Billings
-use: You must include this header.
-depends: jquery
+use: You must include this header. 
 doc: css: .cline{}
      html: <div id="clog"></div>
+     
 */
+var resultPage = "<div class=\"wrapper\"><div class=\"console-tabs\"><span id=\"consoleButton\" class=\"tab selected\">Console</span><span id=\"hButton\" class=\"tab\">Render</span></div><div id=\"clog\" class=\"console-page\"></div></div>";
+$('body').prepend( resultPage );
 
-var consoleLine = "<p class=\"cline\"></p>";
+var consoleLine = "<p class=\"cline\"> </p>";
 
 console = {
   log: function(text) {
@@ -21,3 +23,18 @@ console = {
   }
 };
 
+$("#consoleButton").click(function() {
+  $("#clog").addClass("display-block").removeClass("display-none");
+  $("#consoleButton").addClass("selected");
+  $("#hPage").addClass("display-none").removeClass("display-block");
+  $("#hButton").removeClass("selected");
+  $(".wrapper-shrink").addClass("wrapper").removeClass("wrapper-shrink")
+});
+
+$("#hButton").click(function() {
+  $("#clog").addClass("display-none").removeClass("display-block");
+  $("#consoleButton").removeClass("selected");
+  $(".wrapper").addClass("wrapper-shrink").removeClass("wrapper")
+});
+
+console.log("HTMLConsole.js by Drew Billings");
