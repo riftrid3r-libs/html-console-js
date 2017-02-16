ea0f1ea1aceefbbbc4ff6d631cde1f5f50e26588
dib <dib@dib.p>
2017-02-15 20:19:51 -0800

    try some fix for JSFiddle

--- bin/html-console.js
+++ bin/html-console.js
@@ -15,28 +15,28 @@ doc: css: .cline{}
 $( document ).ready(function(){
   var resultPage = "<div class=\"wrapper\"><div class=\"console-tabs\"><span id=\"consoleButton\" class=\"tab selected\">Console</span><span id=\"hButton\" class=\"tab\">Render</span></div><div id=\"clog\" class=\"console-page\"></div></div>";
   $('body').prepend( resultPage )
+
+  var consoleLine = "<p class=\"cline\"> </p>";
+
+  console = {
+    log: function(text) {
+      $("#clog").append($(cLine).html(text));
+    }
+  };
+
+  $("#consoleButton").click(function() {
+    $("#clog").addClass("display-block").removeClass("display-none");
+    $("#consoleButton").addClass("selected");
+    $("#hPage").addClass("display-none").removeClass("display-block");
+    $("#hButton").removeClass("selected");
+    $(".wrapper-shrink").addClass("wrapper").removeClass("wrapper-shrink")
+  });
+
+  $("#hButton").click(function() {
+    $("#clog").addClass("display-none").removeClass("display-block");
+    $("#consoleButton").removeClass("selected");
+    $(".wrapper").addClass("wrapper-shrink").removeClass("wrapper")
+  });
+
+  console.log("HTMLConsole.js by Drew Billings");
 });
-
-var consoleLine = "<p class=\"cline\"> </p>";
-
-console = {
-  log: function(text) {
-    $("#clog").append($(consoleLine).html(text));
-  }
-};
-
-$("#consoleButton").click(function() {
-  $("#clog").addClass("display-block").removeClass("display-none");
-  $("#consoleButton").addClass("selected");
-  $("#hPage").addClass("display-none").removeClass("display-block");
-  $("#hButton").removeClass("selected");
-  $(".wrapper-shrink").addClass("wrapper").removeClass("wrapper-shrink")
-});
-
-$("#hButton").click(function() {
-  $("#clog").addClass("display-none").removeClass("display-block");
-  $("#consoleButton").removeClass("selected");
-  $(".wrapper").addClass("wrapper-shrink").removeClass("wrapper")
-});
-
-console.log("HTMLConsole.js by Drew Billings");
